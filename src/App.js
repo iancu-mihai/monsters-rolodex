dc11e21d22dab224e214aacc57451f90e4289c92
iancu-mihai <aviancumihai@gmail.com>
2021-05-18 15:44:14 +0300

    Searchbox + Styling
    Created Searchbox component and finished styling the app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -18,17 +19,21 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
-      )
+    )
     return (
       <div className='App'>
-        <input
-          type='search'
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
           placeholder='search monsters'
-          onChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
